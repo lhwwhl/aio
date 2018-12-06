500b6b71a67a7e7c35bff3a65c5cb814d9b0557e
liuhongwei <liuhongwei@ebupt.com>
2018-12-06 14:37:01 +0800

    sync clean code

--- sync_io_read.c
+++ sync_io_read.c
@@ -25,26 +25,24 @@ void read_file_in_blocks(int *fds, int nums, int block) {
     char buf[block];
 
     long long start=0, end=0;
-    int caps=15;
+    long long s_start=0, s_end=0;
     int file_size = get_file_size(fds[0]);
     int ret;
     int offset=0;
     while(file_size>offset) {
         start = get_current_time_ms();
-        //for(int i=0; i<nums; ++i) {
-        for(int i=0; i<caps; ++i) {
-            //start = get_current_time_ms();
+        for(int i=0; i<nums; ++i) {
+            s_start = get_current_time_ms();
             ret = read(fds[i], buf, block);
-            //end = get_current_time_ms();
-            //printf("single read time %d us\n", (int)(end-start));
+            s_end = get_current_time_ms();
+            printf("single read time %d us\n", (int)(s_end-s_start));
         }
         end = get_current_time_ms();
-        printf("fd %d read time %d us\n", caps, (int)(end-start));
+        printf("fd %d read time %d us\n", nums, (int)(end-start));
         offset += block;
-        if(caps < nums)
-            caps += 15;
     }
 }
+
 void test_handle_time(int *fds, int nums, int block, int mode) {
     long long start=0;
     long long end=0;
@@ -60,19 +58,19 @@ void test_handle_time(int *fds, int nums, int block, int mode) {
 }
 
 int main(int argc, char *argv[]) {
-    //if(argc != 5) {
-    //    printf("input: ./exec fd_nums flag block_size mode(0,1)\n");
-    //    return -1;
-    //}
-    //int nums = atoi(argv[1]);
-    //int flags = atoi(argv[2]);
-    //int block = atoi(argv[3]);
-    //int mode = atoi(argv[4]);
+    if(argc != 5) {
+        printf("input: ./exec fd_nums flag block_size mode(0,1)\n");
+        return -1;
+    }
+    int nums = atoi(argv[1]);
+    int flags = atoi(argv[2]);
+    int block = atoi(argv[3]);
+    int mode = atoi(argv[4]);
 
-    int nums = 255;
-    int flags = 1;
-    int block = 4096;
-    int mode = 1;
+    //int nums = 255;
+    //int flags = 1;
+    //int block = 4096;
+    //int mode = 1;
 
     int fds[nums];
     char path[20]={0};
